301a0ebcc9c6a9cef51896121f140827ba13a6fa
Cna.shah <sina.shahhoseinii@gmail.com>
2019-01-05 00:38:27 +0330

    Add Number of rows Add cell for row Add did SelectRow ( Checkmark , Uncheckmard )

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Sina Shahhoseini on 10/14/1397 AP.
+//  Copyright © 1397 cnashah. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["A","B","C"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+//MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+//MARK - Tableview delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+
